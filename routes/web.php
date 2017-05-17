a367cee2a53f97bff2ef5d0b615c718708f94518
Muhamad Anjar <arvanria@gmail.com>
2017-05-17 09:55:09 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,31 @@ Route::get('/', function () {
     return redirect('admin/login');
 });
 
-Auth::routes();
+//Auth::routes();
+
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login');
+Route::post('logout','Auth\LoginController@logout')->name('logout');
+
+Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::post('password/reset','Auth\ResetPasswordController@reset');
+Route::get('password/reset','Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::get('password/reset','Auth\ResetPasswordController@showResetForm')->name('password.request');
+Route::get('register','Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register','Auth\RegisterController@register');
+
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-
 Route::get('provinsi',function ($value=''){
 	$provinsi = \App\Provinsi::orderBy('kode_provinsi')->get();
 	return $provinsi;
 });
-
 Route::get('kecamatan',function ($value=''){
 	$provinsi = \App\Kecamatan::orderBy('kode_kecamatan')->get();
 	return $provinsi;
 });
 
 
-Route::get('map',function ($value=''){
-	return view('map');
-});
 
 
 
